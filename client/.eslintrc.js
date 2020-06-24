53616d8495f1c0167c3bfdc65af49b42bae74fc4
Alexandr <theobroma222@gmail.com>
2020-06-24 11:44:39 +0300

    explicit-function-return-type 0

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: ['airbnb-typescript-prettier', 'plugin:jest/recommended'],
   plugins: ['react-hooks'],
   rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/interface-name-prefix': 0,
     '@typescript-eslint/no-use-before-define': 0,
     'import/prefer-default-export': 0,
