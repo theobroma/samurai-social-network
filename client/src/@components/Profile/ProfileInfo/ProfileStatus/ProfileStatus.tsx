11ce400267f3908effca05c2da68a04cf54063d7
Alexandr Siryj <theobroma222@gmail.com>
2021-09-15 19:45:57 +0300

    usePrevious hook in status

--- client/src/@components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
+++ client/src/@components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
@@ -1,21 +1,39 @@
-import React, { ChangeEvent, FC, useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
+import usePrevious from '../../../../@hooks/usePrevious';
 
 type PropsType = {
   status: string;
   updateStatus: (status: string) => void;
 };
 
-export const ProfileStatus: FC<PropsType> = ({ status, updateStatus }) => {
+export const ProfileStatus: React.FC<PropsType> = ({
+  status,
+  updateStatus,
+}) => {
   const [editMode, setEditMode] = useState<boolean>(false);
   const [statusVal, setStatus] = useState<string>(status);
+  const prevStatusVal = usePrevious(statusVal);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => setStatus(status), [status]);
 
   const activateEditMode = () => setEditMode(true);
+
   const deactivateEditMode = () => {
     setEditMode(false);
-    updateStatus(statusVal);
+    // prevent unnecessary api call
+    if (statusVal !== prevStatusVal) {
+      updateStatus(statusVal);
+    }
+    setEditMode(false);
   };
+
+  // https://stackoverflow.com/a/53315615/3988363
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [editMode]);
 
   const onStatusChange = (e: ChangeEvent<HTMLInputElement>) =>
     setStatus(e.currentTarget.value);
@@ -29,6 +47,7 @@ export const ProfileStatus: FC<PropsType> = ({ status, updateStatus }) => {
         </div>
       ) : (
         <input
+          ref={inputRef}
           onChange={onStatusChange}
           // autoFocus
           onBlur={deactivateEditMode}
---
+++ client/src/@hooks/usePrevious.ts
@@ -0,0 +1,14 @@
+// https://stackoverflow.com/a/57706747/3988363
+import { useRef, useEffect } from 'react';
+
+export default function usePrevious<T>(value: T): T | undefined {
+  // The ref object is a generic container whose current property is mutable ...
+  // ... and can hold any value, similar to an instance property on a class
+  const ref = useRef<T>();
+  // Store current value in ref
+  useEffect(() => {
+    ref.current = value;
+  }, [value]); // Only re-run if value changes
+  // Return previous value (happens before update in useEffect above)
+  return ref.current;
+}
