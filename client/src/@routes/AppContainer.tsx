75e3dd5d1b96816005fd78740f8761d4fdd184f6
Alexandr Siryj <theobroma222@gmail.com>
2021-12-26 14:08:48 +0200

    migtarion to rr6 in general

--- client/src/@routes/AppContainer.tsx
+++ client/src/@routes/AppContainer.tsx
@@ -1,39 +1,23 @@
 // https://stackoverflow.com/questions/54158994/react-suspense-lazy-delay
 import pMinDelay from 'p-min-delay';
 import React, { lazy, Suspense } from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-  Outlet,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoadingPage from '../@components/UI/LoadingPage';
-import { IRoute, ROUTES } from '../@types';
-// import AuthenticatedRoute from './AuthenticatedRoute';
 import { GuestLayout, UserLayout } from './Layouts';
 import RequireAuth from './RequireAuth';
 
 const MIN_LAZY_DELAY = 300;
 
+// Public
 const MainView = lazy(() =>
   pMinDelay(import('../@views/MainView'), MIN_LAZY_DELAY),
 );
 const LoginView = lazy(() =>
   pMinDelay(import('../@views/LoginView'), MIN_LAZY_DELAY),
 );
-const MusicView = lazy(() =>
-  pMinDelay(import('../@views/MusicView'), MIN_LAZY_DELAY),
-);
-const DialogsView = lazy(() =>
-  pMinDelay(import('../@views/DialogsView'), MIN_LAZY_DELAY),
-);
+// Protected
 const ProfileView = lazy(() =>
   pMinDelay(import('../@views/ProfileView'), MIN_LAZY_DELAY),
-);
-const SettingsView = lazy(() =>
-  pMinDelay(import('../@views/SettingsView'), MIN_LAZY_DELAY),
 );
 const UsersView = lazy(() =>
   pMinDelay(import('../@views/UsersView'), MIN_LAZY_DELAY),
@@ -41,62 +25,21 @@ const UsersView = lazy(() =>
 const ChatView = lazy(() =>
   pMinDelay(import('../@views/ChatView'), MIN_LAZY_DELAY),
 );
+
+const MusicView = lazy(() =>
+  pMinDelay(import('../@views/MusicView'), MIN_LAZY_DELAY),
+);
+const DialogsView = lazy(() =>
+  pMinDelay(import('../@views/DialogsView'), MIN_LAZY_DELAY),
+);
+
+const SettingsView = lazy(() =>
+  pMinDelay(import('../@views/SettingsView'), MIN_LAZY_DELAY),
+);
+// Not found page
 const Page404View = lazy(() =>
   pMinDelay(import('../@views/Page404View'), MIN_LAZY_DELAY),
 );
-
-// export const APP_MAIN_ROUTES: IRoute[] = [
-//   {
-//     comp: MainView,
-//     exact: true,
-//     layout: GuestLayout,
-//     onlyPublic: true,
-//     path: ROUTES.ROOT,
-//   },
-//   {
-//     comp: LoginView,
-//     exact: true,
-//     layout: GuestLayout,
-//     onlyPublic: true,
-//     path: ROUTES.LOGIN,
-//   },
-//   {
-//     comp: MusicView,
-//     exact: true,
-//     layout: UserLayout,
-//     path: ROUTES.MUSIC,
-//   },
-//   {
-//     comp: DialogsView,
-//     exact: true,
-//     layout: UserLayout,
-//     path: ROUTES.DIALOGS,
-//   },
-//   {
-//     comp: ProfileView,
-//     exact: true,
-//     layout: UserLayout,
-//     path: ROUTES.PROFILE,
-//   },
-//   {
-//     comp: SettingsView,
-//     exact: true,
-//     layout: UserLayout,
-//     path: ROUTES.SETTINGS,
-//   },
-//   {
-//     comp: UsersView,
-//     exact: true,
-//     layout: UserLayout,
-//     path: ROUTES.USERS,
-//   },
-//   {
-//     comp: ChatView,
-//     exact: true,
-//     layout: UserLayout,
-//     path: ROUTES.CHAT,
-//   },
-// ];
 
 export const AppContainer = () => {
   return (
@@ -117,18 +60,49 @@ export const AppContainer = () => {
                 </RequireAuth>
               }
             />
+            <Route
+              path="users"
+              element={
+                <RequireAuth>
+                  <UsersView />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="chat"
+              element={
+                <RequireAuth>
+                  <ChatView />
+                </RequireAuth>
+              }
+            />
+            {/* Empty pages */}
+            <Route
+              path="music"
+              element={
+                <RequireAuth>
+                  <MusicView />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="dialogs"
+              element={
+                <RequireAuth>
+                  <DialogsView />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="settings"
+              element={
+                <RequireAuth>
+                  <SettingsView />
+                </RequireAuth>
+              }
+            />
           </Route>
-          {/* <Route
-              path="/details/:mediaType/:id"
-              element={<MoviesDetailsView />}
-            /> */}
-          {/* Nested favourites */}
-          {/* <Route path="favourites" element={<FavouritesLayout />}>
-              <Route path="movies" element={<FavouritesMoviesView />} />
-              <Route path="tv" element={<FavouritesTVView />} />
-              <Route index element={<div>Click any tab.</div>} />
-            </Route> */}
-
+          {/* Not found page */}
           <Route path="*" element={<GuestLayout />}>
             <Route path="*" element={<Page404View />} />
           </Route>
--- client/src/@routes/Layouts.tsx
+++ client/src/@routes/Layouts.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-  Outlet,
-} from 'react-router-dom';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Footer from '../@components/Footer';
 import Header from '../@components/Header';
 import Sidebar from '../@components/Sidebar/Sidebar';
-import Footer from '../@components/Footer';
 
 export const UserLayout = () => {
   return (
